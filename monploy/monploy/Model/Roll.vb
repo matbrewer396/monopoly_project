ad44b313a9a17f0cb0cbd670a99dd1bc41985025
Yamto <hiimjames@live.com>
2015-01-02 22:25:54 +0000

    Adds random 6 Dice number + rolltotal

--- monploy/monploy/Model/Roll.vb
+++ monploy/monploy/Model/Roll.vb
@@ -1,26 +1,35 @@
 ﻿Public Class Roll
     Private _Dice1 As String
-    Public Property Dice1() As String
+    Public Property Dice1() As Integer
         Get
             Return _Dice1
         End Get
-        Set(ByVal value As String)
+        Set(ByVal value As Integer)
             _Dice1 = value
         End Set
     End Property
 
-    Private _Dice2 As String
-    Public Property Dice2() As String
+    Private _Dice2 As Integer
+    Public Property Dice2() As Integer
         Get
             Return _Dice2
         End Get
-        Set(ByVal value As String)
+        Set(ByVal value As Integer)
             _Dice2 = value
         End Set
     End Property
-
+    Private Function DiceRoll()
+        Randomize()
+        Dim SngChoice As Single
+        SngChoice = Rnd()
+        SngChoice = SngChoice * 6
+        SngChoice = Math.Ceiling(SngChoice)
+        Return SngChoice
+    End Function
     Function rollTotal() As Integer
-
+        Dim Dicetotal As Integer
+        Dicetotal = _Dice1 + _Dice2
+        Return Dicetotal
     End Function
 
 
