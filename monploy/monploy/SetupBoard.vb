827dbf71273f87e5274ce881d1b20923449344ed
Matthew Brewer <matthew.brewer@lifecycle-software.com>
2015-01-03 00:01:38 +0000

    Setup Board

--- monploy/monploy/SetupBoard.vb
+++ monploy/monploy/SetupBoard.vb
@@ -1,11 +1,32 @@
 ﻿Public Class SetupBoard
+    Dim dtAviableIcons As DataTable = PlayerIcon.AvaibleIcons
+    Dim LoadPlayer As New List(Of Player)
 
     Private Sub SetupBoard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        cmbIcon.DataSource = dtAviableIcons
+        cmbIcon.DisplayMember = "name"
+        cmbIcon.ValueMember = "name"
     End Sub
 
+    Private Sub btnAddPlayer_Click(sender As Object, e As EventArgs) Handles btnAddPlayer.Click
 
-    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles txtPlayerName.TextChanged
-
+        If Not NameIsAvaiable(txtPlayerName.Text) Then
+            MsgBox("Name is already taken, please use a different name.", MsgBoxStyle.Critical, "Name Already Taken")
+            Exit Sub
+        End If
+        LoadPlayer.Add(New Player(txtPlayerName.Text, New PlayerIcon(dtAviableIcons.Rows.Find(cmbIcon.SelectedValue).Item("name") _
+                                                                     , dtAviableIcons.Rows.Find(cmbIcon.SelectedValue).Item("path"))))
+        lstPlayer.Items.Add(txtPlayerName.Text)
     End Sub
+
+    Function NameIsAvaiable(name) As Boolean
+
+        For Each p As Player In LoadPlayer
+            If p.name = name Then
+                Return False
+            End If
+        Next
+        Return True
+    End Function
+
 End Class
